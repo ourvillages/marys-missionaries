7f75dfcd16c0d7e74017d93aa1378a9c2315d1d7
Daniel Ahn <et3rnal.d@gmail.com>
2022-09-23 14:20:52 -0700

    add strapi module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   // https://tailwindcss.nuxtjs.org/getting-started/setup
-  modules: ['@nuxtjs/tailwindcss'],
+  // https://strapi.nuxtjs.org/
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/strapi'],
+  strapi: [
+    {
+      url: process.env.STRAPI_URL || 'http://localhost:1337',
+      prefix: '/api',
+      version: 'v4',
+      cookie: {},
+      cookieName: 'strapi_jwt',
+    },
+  ],
 })
