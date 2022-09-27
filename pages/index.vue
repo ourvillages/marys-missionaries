1b71ecab6728f5986c2459313251b627e0640dbc
Borris <borris.wiria@gmail.com>
2022-09-27 18:02:25 +0400

    v smooth scroll removed

--- pages/index.vue
+++ pages/index.vue
@@ -309,7 +309,7 @@ export default {
 
     <!-- Back To Top -->
     <div class="w-full my-10 flex justify-center">
-      <a v-smooth-scroll data-aos="flip-down" data-aos-delay="150" href="#navbar"
+      <a data-aos="flip-down" data-aos-delay="150" href="#navbar"
         class="px-6 py-3 flex items-center space-x-2 bg-[#FAFAFA] hover:bg-gray-100 hover:shadow-md border border-[#DDDDDD] rounded-md text-gray-700">
         <span>Back to top</span>
         <ArrowUpIcon :size="20" />
