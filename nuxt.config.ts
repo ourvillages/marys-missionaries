0c73cebdcb18636d148881a231e11c8570e116db
Borris <borris.wiria@gmail.com>
2022-09-28 14:01:17 +0400

    add env to donate page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,13 @@ export default defineNuxtConfig({
   // https://strapi.nuxtjs.org/
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/strapi'],
   css: ['aos/dist/aos.css'],
+  runtimeConfig: {
+    public: {
+      giveButterAccountId: process.env.GIVE_BUTTER_ACCOUNT_ID,
+      giveButterElementId: process.env.GIVE_BUTTER_ELEMENT_ID,
+      giveButterCampaignId: process.env.GIVE_BUTTER_CAMPAIGN_ID,
+    },
+  },
   strapi: [
     {
       url: process.env.STRAPI_URL || 'http://localhost:1337',
--- pages/donate.vue
+++ pages/donate.vue
@@ -1,26 +1,24 @@
-<script setup>
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
 useHead({
   script: [
     {
-      src: 'https://js.givebutter.com/elements/latest.js',
-      async: true,
-    },
-    {
-      children: `
-        window.Givebutter=window.Givebutter||function(){(Givebutter.q=Givebutter.q||[]).push(arguments)};Givebutter.l=+new Date;
-        window.Givebutter('setOptions', {
-          "accountId": "DK1UYLR9RVQtlGXd"
-        });
+      children: `window.Givebutter=window.Givebutter||function(){(Givebutter.q=Givebutter.q||[]).push(arguments)};Givebutter.l=+new Date;
+window.Givebutter('setOptions', {
+  "accountId": "${runtimeConfig.public.giveButterAccountId}",
+  "bubble": false
+});
     `,
     },
+    { src: 'https://js.givebutter.com/elements/latest.js', async: true },
   ],
 })
 </script>
 
 <template>
   <div class="h-screen">
-    <div class="aspect-w-16 aspect-h-9">
-      <div givebutter-element-id="1goAwj"></div>
+    <div class="aspect-w-16 aspect-h-9 [&_iframe]:!h-full">
+      <div :givebutter-element-id="$config.public.giveButterElementId"></div>
     </div>
   </div>
 </template>
