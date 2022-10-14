4d902b04599cf7b7661f85c281424c45c099e54c
Borris <borris.wiria@gmail.com>
2022-10-14 09:11:10 +0400

    add auth middleware for main village page

--- pages/main-village.vue
+++ pages/main-village.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-
+definePageMeta({
+  middleware: ['auth'],
+})
 </script>
 
 <template>
   <h1>This is the main village page</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
