572e89edd210e658f7e635f9527febefc0847639
Daniel Ahn <et3rnal.d@gmail.com>
2022-10-13 20:50:16 -0700

    Wired up the login button
    Co-authored-by: Borris <borris.wiria@gmail.com>

--- components/base/Navbar.vue
+++ components/base/Navbar.vue
@@ -1,12 +1,23 @@
 <template>
   <nav id="navbar" class="relative z-10 w-full text-neutral-800">
-    <div class="flex flex-col max-w-screen-xl px-8 mx-auto lg:items-center lg:justify-between lg:flex-row py-4">
-      <div class="flex flex-col lg:flex-row items-center space-x-4 xl:space-x-8">
+    <div
+      class="flex flex-col max-w-screen-xl px-8 mx-auto lg:items-center lg:justify-between lg:flex-row py-4"
+    >
+      <div
+        class="flex flex-col lg:flex-row items-center space-x-4 xl:space-x-8"
+      >
         <div class="w-full flex flex-row items-center justify-between py-6">
           <NuxtLink to="/">
-            <img src="/img/logo/nefa.svg" class="w-24 xl:w-28" alt="Nefa Logo" />
+            <img
+              src="/img/logo/nefa.svg"
+              class="w-24 xl:w-28"
+              alt="Nefa Logo"
+            />
           </NuxtLink>
-          <button class="rounded-lg lg:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
+          <button
+            class="rounded-lg lg:hidden focus:outline-none focus:shadow-outline"
+            @click="open = !open"
+          >
             <Icon name="mdi:segment" v-if="!open" size="24" />
             <Icon name="mdi:close" v-else size="24" />
           </button>
@@ -29,18 +40,37 @@
               <Icon name="mdi:chevron-down" v-else size="20" />
             </button>
             <transition name="transform-fade-down">
-              <ul v-if="dropdownNavbar" class="flex lg:absolute flex-col max-w-42 py-1 lg:bg-white rounded-md lg:shadow-md pl-2 lg:pl-0">
+              <ul
+                v-if="dropdownNavbar"
+                class="flex lg:absolute flex-col max-w-42 py-1 lg:bg-white rounded-md lg:shadow-md pl-2 lg:pl-0"
+              >
                 <li>
-                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Exchange</a>
+                  <a
+                    href="#"
+                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
+                    >Exchange</a
+                  >
                 </li>
                 <li>
-                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Wallet</a>
+                  <a
+                    href="#"
+                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
+                    >Wallet</a
+                  >
                 </li>
                 <li>
-                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Explorer</a>
+                  <a
+                    href="#"
+                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
+                    >Explorer</a
+                  >
                 </li>
                 <li>
-                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Charts</a>
+                  <a
+                    href="#"
+                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100"
+                    >Charts</a
+                  >
                 </li>
               </ul>
             </transition>
@@ -48,8 +78,18 @@
         </ul>
       </div>
       <div :class="[open ? 'flex' : 'hidden lg:flex']" class="space-x-3">
-        <base-button class="px-8 xl:px-10 py-3 mt-2 bg-inherit text-gradient border border-[#0c66ee]"> Login </base-button>
-        <base-button class="px-8 xl:px-10 py-3 mt-2 bg-gradient-to-r from-[#468ef9] to-[#0c66ee] text-white"> Sign Up </base-button>
+        <NuxtLink to="/login">
+          <base-button
+            class="px-8 xl:px-10 py-3 mt-2 bg-inherit text-gradient border border-[#0c66ee]"
+          >
+            Login
+          </base-button>
+        </NuxtLink>
+        <base-button
+          class="px-8 xl:px-10 py-3 mt-2 bg-gradient-to-r from-[#468ef9] to-[#0c66ee] text-white"
+        >
+          Sign Up
+        </base-button>
       </div>
     </div>
   </nav>
